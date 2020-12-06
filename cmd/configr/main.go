680a51dc2e4b3f5cc0048c543f075a3d0e712ae8
Darrien Kennedy <kennedydarrien@gmail.com>
2020-12-05 23:49:22 -0500

    Read first element of files list in inventory file from configrPath variable.

--- cmd/configr/main.go
+++ cmd/configr/main.go
@@ -7,15 +7,28 @@ import (
 )
 
 func main() {
-	var inventory = NewInventory("file.json")
+	// TODO: allow user to specify via environment variable the path
+	//       to their inventory file.
+	var configrPath, _ = os.UserHomeDir()
+	configrPath += "/.configr.json"
+
+	// If configrPath file doesn't exist, exit with failure.
+	// TODO: Create a template file if the file doesnt exist with default
+	//       configuration.
+	if _, err := os.Stat(configrPath); os.IsNotExist(err) {
+		fmt.Printf("Err: could not find file at %s\n", configrPath)
+		os.Exit(1)
+	}
+
+	var inventory = NewInventory(configrPath)
 	var selectedFile = inventory.Files[0]
 	var fd = NewFileData(selectedFile)
 
-	editFile(*fd)
+	editFile(fd.Path)
 }
 
-func editFile(fd FileData) {
-	var cmd = exec.Command("vim", fd.Path)
+func editFile(filePath string) {
+	var cmd = exec.Command("vim", filePath)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
